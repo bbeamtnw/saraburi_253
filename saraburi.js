7dc2d22fd6caf195d327494ebe3a9e06389dcd95
bbeamtnw <124548096+bbeamtnw@users.noreply.github.com>
2023-04-24 11:29:31 +0700

    Add files via upload

---
+++ saraburi.js
@@ -0,0 +1,155 @@
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อุทยานแห่งชาติน้ำตกสามหลั่",
+          "type": "ธรรมชาติ",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.92744285113605,
+            14.489974266359468
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "โลตัส สระุบรี",
+          "type": "ห้างสรรพสินค้า",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.92206676724635,
+            14.522012375148961
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สุขอนันต์ปาร์ค สระบุรี",
+          "type": "ห้างสรรพสินค้า",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.92520279609522,
+            14.524994823401926
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ทวีกิจ คอมเพล็กซ์ สระบุรี",
+          "type": "ห้างสรรพสินค้า",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.91781557298486,
+            14.523765375649731
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดทองพุ่มพวง",
+          "type": "วัฒนธรรม",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.91086262341605,
+            14.531625556565999
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดศาลาแดง",
+          "type": "วัฒนธรรม",
+          "level": 6
+        },
+        "geometry": {
+          "coordinates": [
+            100.909905068194,
+            14.529315614574742
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "บ้านดารารีสอร์ท ",
+          "type": "รีสอร์ท",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.9431812196944,
+            14.5183223844097
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "SK Grand Mart",
+          "type": "ห้างสรรพสินค้า",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.89348774575552,
+            14.431823187309476
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สนามกีฬาเทศบาลตำบลหนองแค",
+          "type": "สนามกีฬา",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.87582503146746,
+            14.329656666208564
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ผาไทร็อค",
+          "type": "ธรรมชาติ",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.88266372407338,
+            14.471527242784134
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
---
+++ tourpoint.js
@@ -0,0 +1,80 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+        "features": [
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Bangkok",
+              "type": "culture",
+              "level": 10
+            },
+            "geometry": {
+              "coordinates": [
+                100.49372093212742,
+                13.753471918317317
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Pathumthani",
+              "type": "culture",
+              "level": 7
+            },
+            "geometry": {
+              "coordinates": [
+                100.53296449640948,
+                14.02014745589976
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Nakhonpathom",
+              "type": "culture",
+              "level": 6
+            },
+            "geometry": {
+              "coordinates": [
+                100.0620417250185,
+                13.819303448359705
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Ratchaburi",
+              "type": "natural",
+              "level": 6
+            },
+            "geometry": {
+              "coordinates": [
+                99.82111203404418,
+                13.536880124238394
+              ],
+              "type": "Point"
+            }
+          },
+          {
+            "type": "Feature",
+            "properties": {
+              "name": "Kanchanaburi",
+              "type": "natural",
+              "level": 9
+            },
+            "geometry": {
+              "coordinates": [
+                99.53577986409715,
+                14.020064558664657
+              ],
+              "type": "Point"
+            }
+          }
+        ]
+      }
